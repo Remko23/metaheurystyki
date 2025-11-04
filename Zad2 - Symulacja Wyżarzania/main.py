28258333b68568a84bcd2b08095c6eb11c817763
Remko23 <r-tomecki.live@wp.pl>
2025-11-04 20:36:35 +0100

    Dzialajace parametry

--- Zad2 - Symulacja Wyżarzania/main.py
+++ Zad2 - Symulacja Wyżarzania/main.py
@@ -2,7 +2,32 @@ import random as r
 from math import exp, sin, pi
 import time
 import numpy as np
-import pandas as pd
+import json
+
+def menu():
+    print("1 - Podaj parametry i wykonaj dla nich symulowane wyzarzanie")
+    print("2 - Przeprowadz symulację dla różnych wartości wybranego parametru")
+    wybor = int(input("Twój wybór: "))
+    if wybor == 1:
+        T,M,N,ochl,k = pobierz_parametry()
+        best, bestit, wyl_rozw = symulowane_wyzarzanie(M, N, start, koniec, T, ochl, k ,f)
+        print(f"x = {best}\nf(x) = {f(best)}\nIteracji do znalezienia rozw = {bestit}\nWylosowane pierwsze rozwiazanie = {wyl_rozw}")
+    elif wybor == 2:
+        symulacja_symulowanego_wyzarzania(f, start, koniec)
+
+def pobierz_parametry():
+    T0 = float(input("Podaj temperaturę początkową T0: "))
+    ochl = 1
+    while ochl >= 1 or ochl <= 0:
+        ochl = float(input("Podaj wartosc ochladzania z zakresu (0,1): "))
+    l_e = 0
+    l_p = 0
+    while l_e < 1:
+        l_e = int(input("Podaj liczbę epok (M): "))
+    while l_p < 1:
+        l_p = int(input("Podaj liczbę prób (N): "))
+    k = float(input("Podaj stałą Boltzmanna (k): "))
+    return T0, l_e, l_p, ochl, k
 
 def alfa(T, ochlodzenie):
     return T*ochlodzenie;
@@ -42,7 +67,6 @@ def f2(x):
 def P(rozw, rozw2, T, k):
     if(T==0):
         return 0.0
-
     return exp((f(rozw2)-f(rozw))/(k*T))
 
 def symulowane_wyzarzanie(l_epok, l_prob, start, koniec, T, ochlodzenie, k, f):
@@ -86,35 +110,59 @@ def symulowane_wyzarzanie(l_epok, l_prob, start, koniec, T, ochlodzenie, k, f):
 
 def symulacja_symulowanego_wyzarzania(f, start, koniec):
     i = 0
-    results = []
-    PARAMS = {
-        'T': [50, 100, 200, 500, 1000],
-        'M': [5, 20, 30, 50, 100],
-        'N': [5, 20, 30, 50, 100],
-        'alfa': [0.7, 0.8, 0.9, 0.95, 0.99],
-        'k': [0.1, 0.3, 0.5, 0.8, 1]
+    all_results_json = []
+    par = -1
+    while par not in [0, 1, 2, 3, 4]:
+        par = int(input("Wybierz parametr do symulacji: \nT - 0 \nM - 1\nN - 2\nalfa - 3\nk - 4\nTwój wybór: "))
+    par_keys = ['T', 'M', 'N', 'alfa', 'k']
+    key = par_keys[par]
+    VALUES = {
+        'T': [1000, 20, 50, 100, 200, 300, 400, 500, 700, 900],
+        'M': [100, 5, 10, 20, 30, 50, 200, 500, 700, 1000],
+        'N': [100, 5, 10, 20, 30, 50, 200, 500, 700, 1000],
+        'alfa': [0.85, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.97, 0.99],
+        'k': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
     }
-    it = [0,0,0,0,0]
-    pom = 1
-    # wykonujemy 5 razy dla tych samych parametrow, aby sprawdzić stabilność algorytmu dla wylosowywanych pierwszych rozwiazan
-    while i < 5:
-        start_time = time.time()
+    PARAMS = {
+        'T': VALUES['T'][0], #domyslnie: 1000
+        'M': VALUES['M'][0], #domyslnie: 100
+        'N': VALUES['N'][0], #domyslnie: 100
+        'alfa': VALUES['alfa'][0], #domyslnie: 0.85
+        'k': VALUES['k'][0], #domyslnie: 0.1
+    }
+    p = 0
+    while p < len(VALUES[key]):
+        PARAMS[key] = VALUES[key][p]
+        current_param_results = []
+        # wykonujemy 5 razy dla tych samych parametrow, aby sprawdzić stabilność algorytmu dla wylosowywanych pierwszych rozwiazan
+        while i < 5:
+            start_time = time.time()
+            best_rozw, best_it, pierwsze_rozw = symulowane_wyzarzanie(PARAMS['M'], PARAMS['N'], start, koniec, PARAMS['T'], PARAMS['alfa'], PARAMS['k'], f)
+            end_time = time.time()
+            duration = end_time - start_time
+            result_dict = {
+                'best_rozw': best_rozw,
+                'f(x)': f(best_rozw),
+                'best_it': best_it,
+                'poczatkowe_rozw': pierwsze_rozw,
+                'czas': duration,
+                'nr_iteracji': i + 1,
+                'parametry': {
+                    'T': PARAMS['T'],
+                    'M': PARAMS['M'],
+                    'N': PARAMS['N'],
+                    'alfa': PARAMS['alfa'],
+                    'k': PARAMS['k']
+                }
+            }
+            current_param_results.append(result_dict)
+            i += 1
 
-        best_rozw, best_it, pierwsze_rozw = symulowane_wyzarzanie(PARAMS['M'][0], PARAMS['N'][0], start, koniec, PARAMS['T'][0], PARAMS['alfa'][0], PARAMS['k'][0],f)
-
-        end_time = time.time()
-        duration = end_time - start_time
-
-        results.append([best_rozw, f(best_rozw), best_it, pierwsze_rozw, duration, i+1, PARAMS['T'][0], PARAMS['M'][0], PARAMS['N'][0], PARAMS['alfa'][0], PARAMS['k'][0]])
-
-        # Format pliku wynikowego:
-        # [rozw, wartosc f(rozw), liczba iteracji do znalezienia najlepszego, wylosowane pierwsze rozw, czas trwania, numer iteracji (1-5), T, M, N, alfa, k]
-        if f == f1:
-            np.save('output.npy', np.array(results))
-        elif f==f2:
-            np.save('output2.npy', np.array(results))
-        i += 1
-
+        all_results_json.extend(current_param_results)
+        filename = f'{key}_output_f1.json' if f == f1 else f'{key}_output_f2.json'
+        with open(filename, 'w') as file:
+            json.dump(all_results_json, file, indent=4)
+        p+=1
 
 f, start, koniec = wybierz_funkcje()
-symulacja_symulowanego_wyzarzania(f, start, koniec)
+menu()
--- Zad2 - Symulacja Wyżarzania/output_analyser.py
+++ Zad2 - Symulacja Wyżarzania/output_analyser.py
@@ -1,7 +1,12 @@
-import numpy as np
+import json
 
-data = np.load("output.npy")
-print(data)
-print("---------------------------------------------------------------------------")
-data2 = np.load("output2.npy")
-print(data2)
+def wczytaj_dane_z_json(nazwa_pliku):
+    with open(nazwa_pliku, 'r') as file:
+        dane_json = json.load(file)
+        return dane_json
+
+nazwa_pliku = 'T_output_f1.json'
+dane = wczytaj_dane_z_json(nazwa_pliku)
+
+print(json.dumps(dane, indent=4))
+print(dane[0]['Parametry_Aktualne']['T0'])
